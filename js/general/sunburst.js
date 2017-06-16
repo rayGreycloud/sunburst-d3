39d81302d86935c46b4cdc81086b81d7a61f42c2
rayGreycloud <rayGreycloud@gmail.com>
2017-06-16 09:02:24 -0700

    Add properly rotated labels to sunburst

--- js/general/sunburst.js
+++ js/general/sunburst.js
@@ -37,17 +37,47 @@ function sunburst() {
       .innerRadius(function (d) { return d.y0})
       .outerRadius(function (d) { return d.y1});
 
-    // Put pieces together
-    g.selectAll('path')
+    // Add <g> element for each node
+    g.selectAll('g')
       .data(root.descendants())
       .enter()
-      .append('path')
-      .attr('display', function (d) { return d.depth ? null : "none"; })
+      .append('g').attr('class', 'node')
+      // Add path elements
+      .append('path').attr('display', function (d) { return d.depth ? null : "none"; })
+      // Draw paths
       .attr('d', arc)
+      // Color lines
       .style('stroke', '#fff')
+      // Color slices
       .style('fill', function (d) {
         return color((d.children ? d : d.parent).data.name);
       });
+
+    // Add label for each node
+    g.selectAll('.node')
+      // Add empty text element to each node
+      .append('text')
+      // Move ref point for text element to center of arc
+      .attr('transform', function (d) {
+        return `translate(${arc.centroid(d)})rotate( ${computeTextRotation(d)})`;
+      })
+      // Move text element to left to look centered
+      .attr('dx', '-20')
+      // Pull text element closer to center
+      .attr('dy', '.5em')
+      // Get name attribute for each node
+      .text(function (d) {
+        return d.parent ? d.data.name : ''
+      });
+    // Calculate distance to rotate each label
+    function computeTextRotation(d) {
+      const angle = (d.x0 + d.x1) / Math.PI * 90;
+      // Avoid upside down labels
+      // Labels as rims
+      return (angle < 120 || angle > 270) ? angle : angle + 180;
+      // alternate - labels as spokes
+      // return (angle < 180) ? angle - 90 : angle + 90;
+    }
   });
 
 }
